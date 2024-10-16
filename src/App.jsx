21816a66c4b5365a50a715d3388130bdb6d50bd9
lucasmclaughlin <lucas.mclaughlin@gmail.com>
2024-10-16 21:34:22 +0100

    cleaned up styles

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,22 @@ import './App.css'; // Importing styles
 
 function App() {
   const [todos, setTodos] = useState([]);
+  const [futureTodos, setFutureTodos] = useState([]); // Future to-dos
   const [todoValue, setTodoValue] = useState('');
   const [currentTodo, setCurrentTodo] = useState('');
+  const [currentTodoIndex, setCurrentTodoIndex] = useState(null); // Track index of current random to-do
   const [view, setView] = useState('random'); // Default view is random
   const [isDarkMode, setIsDarkMode] = useState(false); // Light or Dark mode
   const [timeLeft, setTimeLeft] = useState(1500); // Pomodoro timer (25 min)
   const [isTimerRunning, setIsTimerRunning] = useState(false); // Track if timer is running
 
-  function persistData(newList) {
-    localStorage.setItem('todos', JSON.stringify({ todos: newList }));
+  function persistData(newList, futureList) {
+    localStorage.setItem('todos', JSON.stringify({ todos: newList, futureTodos: futureList }));
   }
 
   function handleAddTodos(newTodo) {
     const newTodoList = [...todos, newTodo];
-    persistData(newTodoList);
+    persistData(newTodoList, futureTodos);
     setTodos(newTodoList);
   }
 
@@ -26,12 +28,45 @@ function App() {
     const newTodoList = todos.filter((todo, todoIndex) => {
       return todoIndex !== index;
     });
-    persistData(newTodoList);
+    persistData(newTodoList, futureTodos);
     setTodos(newTodoList);
     if (newTodoList.length > 0) {
       getRandomTodo(newTodoList);
     } else {
       setCurrentTodo('');
+    }
+  }
+
+  // Mark a todo as "done" and move to future todos list
+  function handleDoneTodo(index) {
+    const todoToMove = todos[index];
+    const newFutureTodos = [...futureTodos, { todo: todoToMove, doneDate: new Date() }];
+    const newTodoList = todos.filter((_, todoIndex) => todoIndex !== index);
+    persistData(newTodoList, newFutureTodos);
+    setTodos(newTodoList);
+    setFutureTodos(newFutureTodos);
+
+    // After marking the current todo as done, get a new random todo
+    if (newTodoList.length > 0) {
+      getRandomTodo(newTodoList);
+    } else {
+      setCurrentTodo('');
+    }
+  }
+
+  // Move back any todos that were done over 2 days ago
+  function moveFutureTodosBack() {
+    const twoDaysInMillis = 2 * 24 * 60 * 60 * 1000;
+    const now = new Date();
+
+    const readyTodos = futureTodos.filter((item) => now - new Date(item.doneDate) > twoDaysInMillis);
+    const remainingFutureTodos = futureTodos.filter((item) => now - new Date(item.doneDate) <= twoDaysInMillis);
+
+    if (readyTodos.length > 0) {
+      const newTodoList = [...todos, ...readyTodos.map((item) => item.todo)];
+      setTodos(newTodoList);
+      setFutureTodos(remainingFutureTodos);
+      persistData(newTodoList, remainingFutureTodos);
     }
   }
 
@@ -41,12 +76,15 @@ function App() {
     handleDeleteTodo(index);
   }
 
+  // Modified to track the index of the selected random todo
   function getRandomTodo(todoList) {
     if (todoList.length > 0) {
       const randomIndex = Math.floor(Math.random() * todoList.length);
       setCurrentTodo(todoList[randomIndex]);
+      setCurrentTodoIndex(randomIndex);
     } else {
       setCurrentTodo('');
+      setCurrentTodoIndex(null);
     }
   }
 
@@ -55,11 +93,12 @@ function App() {
     const hour = new Date().getHours();
     setIsDarkMode(hour > 18 || hour < 6); // Dark mode after 6pm or before 6am
 
-    let localTodos = localStorage.getItem('todos');
-    if (localTodos) {
-      localTodos = JSON.parse(localTodos).todos;
-      setTodos(localTodos);
-      getRandomTodo(localTodos);
+    let storedTodos = localStorage.getItem('todos');
+    if (storedTodos) {
+      const parsed = JSON.parse(storedTodos);
+      setTodos(parsed.todos);
+      setFutureTodos(parsed.futureTodos || []);
+      getRandomTodo(parsed.todos);
     }
 
     // Pomodoro timer logic
@@ -68,6 +107,9 @@ function App() {
         setTimeLeft((prevTime) => prevTime - 1);
       }
     }, 1000);
+
+    // Check future todos every time the app loads
+    moveFutureTodosBack();
 
     return () => clearInterval(timer); // Cleanup the interval on component unmount
   }, [isTimerRunning, timeLeft]);
@@ -113,6 +155,7 @@ function App() {
             handleEditTodo={handleEditTodo}
             handleDeleteTodo={handleDeleteTodo}
             todos={todos}
+            handleDoneTodo={handleDoneTodo} // Add the done button to each todo
           />
         )}
       </div>
@@ -121,8 +164,11 @@ function App() {
       {view === 'random' && (
         <div className="random-todo-container slide-in">
           <div className="todo-card">
-            {currentTodo ? <p>{currentTodo}</p> : <p>No to-dos available</p>}
+            {currentTodo ? <p>{currentTodo}</p> : <p>unBusy</p>}
           </div>
+          <button onClick={() => handleDoneTodo(currentTodoIndex)} className="done-button">
+            Done
+          </button>
           <button onClick={() => getRandomTodo(todos)} className="random-button">
             Show Another To-Do
           </button>
@@ -130,7 +176,7 @@ function App() {
       )}
 
       {/* Pomodoro Timer */}
-      <div className="pomodoro-timer">
+      {/* <div className="pomodoro-timer">
         <h3>Pomodoro Timer</h3>
         <p>{formatTime(timeLeft)}</p>
         <div className="timer-controls">
@@ -138,7 +184,7 @@ function App() {
           <button onClick={pauseTimer} className="timer-button">Pause</button>
           <button onClick={resetTimer} className="timer-button">Reset</button>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,19 +2,21 @@ import React from 'react'
 import TodoCard from './TodoCard'
 
 export default function TodoList(props) {
-    const { todos } = props
+    const { todos, handleDoneTodo } = props; // Destructure handleDoneTodo from props
     
     return (
         <ul className='main'>
             {todos.map((todo, todoIndex) => {
                 return (
-                    <TodoCard {...props}key={todoIndex} index={todoIndex}>
+                    <TodoCard {...props} key={todoIndex} index={todoIndex}>
                         <p>{todo}</p>
+                        {/* "Done" button to mark the todo as done */}
+                        <button onClick={() => handleDoneTodo(todoIndex)} className="done-button">
+                            Done
+                        </button>
                     </TodoCard>
                 )
             })}
-            
-            
         </ul>
     )
 }
